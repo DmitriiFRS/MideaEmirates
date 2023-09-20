b49257d4b28fb158e302ef4c3ae2ce7ee0e5909b
Дмитрий <Dmitriifrs@gmail.com>
2023-09-20 09:31:07 +0500

    first

---
+++ app/Header/Header.tsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.scss';
+import Image from 'next/image';
+import MideaLogo from '../../public/icons/MainLogo.png';
+import Link from 'next/link';
+
+function Header() {
+   return (
+   <header className={styles.header}>
+      <Link href={'/'}>
+         <Image src={MideaLogo} alt='Midea-CAC logo' width={256} height={90} priority={true} />
+      </Link>
+   </header>
+   )
+}
+export default Header;
